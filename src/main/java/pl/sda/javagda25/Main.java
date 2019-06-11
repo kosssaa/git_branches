5144d25a2eaf748eef0223a9e4e8d77a36d1e5e6
Java <java@MacBook-Air-KOSA.local>
2019-06-11 18:39:09 +0200

    Odczyt

--- src/main/java/pl/sda/javagda25/Main.java
+++ src/main/java/pl/sda/javagda25/Main.java
@@ -1,20 +1,21 @@
 package pl.sda.javagda25;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) {
 
 
-        try {
-            PrintWriter printWriter = new PrintWriter(new FileWriter("data.txt", true));
-            printWriter.println("Hello World!");
+        int counter = 0;
+        try (BufferedReader reader = new BufferedReader(new FileReader("data.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null)
+                counter++;
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println("Liczba linii " + counter);
 
     }
 
