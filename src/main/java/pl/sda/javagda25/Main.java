e537a32c9328ff35dba3429fe3a2cec9ce7154ad
Java <java@MacBook-Air-KOSA.local>
2019-06-11 18:12:22 +0200

    Pierwszy krok

--- src/main/java/pl/sda/javagda25/Main.java
+++ src/main/java/pl/sda/javagda25/Main.java
@@ -1,5 +1,22 @@
 package pl.sda.javagda25;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Main {
+    public static void main(String[] args) {
+
+
+        try {
+            PrintWriter printWriter = new PrintWriter(new FileWriter("data.txt", true));
+            printWriter.println("Hello World!");
+            printWriter.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
